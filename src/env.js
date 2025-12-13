98c3c4495b632892716650c763c3c4b9ab554b28
FrostLegend <alejandro11valla@gmail.com>
2025-12-13 20:17:22 +0100

    arreglar vercel

---
+++ src/env.js
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = "https://vjunfxedwdmoibnfbzoi.supabase.co";
+export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqdW5meGVkd2Rtb2libmZiem9pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1MzIwNjUsImV4cCI6MjA3NjEwODA2NX0.1l8AI_vIcoGZVJzQ7SL15QihGav_VQCnxqFZp854ZV8";
+export const USER_RUTE = "https://vjunfxedwdmoibnfbzoi.supabase.co/auth/v1/"
